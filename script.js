5a88a1dd3a2cebe1fdfb392daa22477855bbcc8f
Marcella <marcellasasso@gmail.com>
2019-08-01 10:36:41 -0300

    Background blue no estilo

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log('Script Funcionou');
+
+
+// git add -A
+
+// git commit -m 'nome'
+
+// adicionar diretório remoto
+// $ git remote add origin
+
+// push
+// $ git push -u origin master 
+
+// git status
+
+// Branch
+// ramificações -> podemos trabalhar em funcionalidades adicionais 
+// para um projeto, sem modificarmos o 'ramificação principal', o master
+
+// adiciona
+// git branch nomebranch
+// mostra todos branch
+// git branch
+// muda de branch
+// git checkout nomebranch
+
+
